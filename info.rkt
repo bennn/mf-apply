496ebd0ab606b9b0ee6c82960ee08fdbe3535427
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-11 12:24:45 -0500

    ** fix pkg deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "mf-apply")
 (define deps '("base" "redex-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "redex-doc"))
 (define pkg-desc "Redex syntax for applying metafunction")
 (define version "1.0")
 (define pkg-authors '(ben))
