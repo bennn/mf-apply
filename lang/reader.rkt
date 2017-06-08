21f252bb80cc340a543f456b9e9aed834557b737
Ben Greenman <benjaminlgreenman@gmail.com>
2017-06-08 01:32:21 -0400

    change meta-reader path-desc-str, for better error messages ?

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -16,7 +16,7 @@
 (define-values (mf-apply-read mf-apply-read-syntax mf-apply-get-info)
   (make-meta-reader
     'mf-apply
-    "language path"
+    "mf-apply"
     (λ (bstr)
       (let* ([str (bytes->string/latin-1 bstr)]
              [sym (string->symbol str)])
